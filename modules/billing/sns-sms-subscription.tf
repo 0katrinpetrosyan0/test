03c2776e4aa4518ee9dd439b2f0fc62148ba7b3d
0katrinpetrosyan0 <katrinpetrosyan321@gmail.com>
2022-07-20 14:23:10 +0400

    change paths

--- modules/billing/sns-sms-subscription.tf
+++ modules/billing/sns-sms-subscription.tf
@@ -8,6 +8,7 @@ resource "aws_sns_topic" "alerts_notify_sms" {
 resource "aws_sns_topic_subscription" "sms" {
   count = length(var.sns_subscription.sns_subscription_phone_number_list)
 
+  
   topic_arn = aws_sns_topic.alerts_notify_sms[0].arn
   protocol  = "sms"
   endpoint  = element(var.sns_subscription.sns_subscription_phone_number_list, count.index)
